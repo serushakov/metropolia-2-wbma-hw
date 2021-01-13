b63fbb2fd9da7c556808c82dc631f67c203f1cc7
Sergey Ushakov <sergey@ushakov.io>
2021-01-13 19:18:44 +0200

    added a header

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import React from "react";
-import { SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import List from "./components/List";
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <List />
+      <ScrollView>
+        <View style={styles.header}>
+          <Image
+            style={styles.headerImage}
+            source={{
+              uri:
+                "https://thumbs.dreamstime.com/b/kitty-cat-adoption-white-background-54990090.jpg",
+            }}
+          />
+          <Text style={styles.countText}>3 homeless cats</Text>
+        </View>
+        <List />
+      </ScrollView>
       <StatusBar />
     </SafeAreaView>
   );
@@ -17,6 +37,26 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  header: {
+    height: 250,
+    width: "100%",
+    marginBottom: 16,
+  },
+  headerImage: {
+    height: "100%",
+    width: "100%",
+  },
+
+  countText: {
+    position: "absolute",
+    bottom: 16,
+    end: 0,
+    backgroundColor: "white",
+    padding: 8,
+    paddingEnd: 24,
+    borderColor: "#acacac",
+    borderWidth: 1,
   },
 });
 
