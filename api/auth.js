cc958beffc9149f2476781adf0b7113d8353f01b
Sergey Ushakov <sergey@ushakov.io>
2021-01-23 11:24:53 +0200

    added a function to post login

---
+++ api/auth.js
@@ -0,0 +1,17 @@
+import { apiUrl } from "./constants";
+
+export async function postLogin(username, password) {
+  const url = apiUrl("/login");
+
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  });
+
+  const content = await response.json();
+
+  return content;
+}
